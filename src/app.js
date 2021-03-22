c63c88285c1632cd86e1c30918477f159309f24b
AvetisDN <avetisdn@gmail.com>
2021-03-22 17:17:52 +0300

    '1'

--- src/app.js
+++ src/app.js
@@ -3,5 +3,16 @@ import Glide from '@glidejs/glide'
 
 new Glide('.glide', {
     type: 'carousel',
-    perView: 3
+    perView: 3,
+    breakpoints: {
+        768: {
+            perView: 1
+        },
+        992: {
+            perView: 2
+        },
+        1200: {
+            perView: 3
+        }
+    }
 }).mount()

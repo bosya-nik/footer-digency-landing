978b096dff0d2702f5f54c321d33f8ba08d4e6f5
AvetisDN <avetisdn@gmail.com>
2021-03-23 18:00:56 +0300

    '1

--- src/app.js
+++ src/app.js
@@ -16,3 +16,29 @@ new Glide('.glide', {
         }
     }
 }).mount()
+new Glide('.glide-blog', {
+    type: 'carousel',
+    perView: 2,
+    breakpoints: {
+        540: {
+            perView: 1
+        },
+        992: {
+            perView: 2
+        }
+    }
+}).mount()
+
+const planSwitcher = document.getElementById('subscription-switcher')
+const pricingMonthly = document.getElementById('monthly')
+const pricingAnnual = document.getElementById('annual')
+
+planSwitcher.onchange = function(e) {
+    if(this.checked) {
+        pricingAnnual.classList.remove('d-none')
+        pricingMonthly.classList.add('d-none')
+    } else {
+        pricingAnnual.classList.add('d-none')
+        pricingMonthly.classList.remove('d-none')
+    }
+}
